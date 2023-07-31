7c49c34194d1e811a22ae4d59799226d9f81b674
Sreevarshan S <101659129+Varshan00@users.noreply.github.com>
2023-07-31 20:46:37 +0530

    Create AddOnsList.jsx

---
+++ reactapp/src/components/Customer/AddOnsList.jsx
@@ -0,0 +1,31 @@
+import UserAddOnItems from './UserAddOnItems';
+
+const AddOnsList = (addons) => {
+  return (
+    <div>
+      {addons.items.map((allAddons) => {
+
+        const [data, speed, ...remainingSubstringArray] = allAddons.addonDetails.split(" ");
+        const note = remainingSubstringArray.join(" ");
+
+        const formattedData = data.toUpperCase() ;
+        const formattedSpeed = speed.toUpperCase() ;
+
+        return (
+          <UserAddOnItems
+            id={allAddons.addonId}
+            type={allAddons.addonType}
+            name={allAddons.addonName}
+            validity={allAddons.addonValidity}
+            data={formattedData}
+            speed={formattedSpeed}
+            note={note}
+            price={allAddons.addonPrice}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default AddOnsList;
