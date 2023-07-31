44da7451ec609f3a2ca0c482a66573dbd2ac54ce
Sreevarshan S <101659129+Varshan00@users.noreply.github.com>
2023-07-31 21:34:25 +0530

    Delete springapp/src/main/java/com/examly/springapp directory

--- springapp/src/main/java/com/examly/springapp/src/main/java/com/examly/springapp/repository/AddOnsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.Model.AddOns;
-
-public interface  AddOnsRepository extends JpaRepository<AddOns,Integer> {
-
-}
--- springapp/src/main/java/com/examly/springapp/src/main/java/com/examly/springapp/repository/RechargeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.Model.Recharges;
-
-public interface RechargeRepository  extends JpaRepository<Recharges,Integer> {
-
-}
