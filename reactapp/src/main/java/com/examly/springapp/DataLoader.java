8c4c050c1d47c0f8cae605b2107c080730ad28d8
Sreevarshan S <101659129+Varshan00@users.noreply.github.com>
2023-07-31 21:19:40 +0530

    Create DataLoader.java

---
+++ reactapp/src/main/java/com/examly/springapp/DataLoader.java
@@ -0,0 +1,92 @@
+package com.examly.springapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import com.examly.springapp.repository.AdminRepository;
+import com.examly.springapp.repository.UserRepository;
+import com.examly.springapp.model.AdminModel;
+import com.examly.springapp.model.UserModel;
+
+@Component
+public class DataCleanup implements CommandLineRunner {
+
+    private final AdminRepository adminRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public DataCleanup(AdminRepository adminRepository, UserRepository userRepository) {
+        this.adminRepository = adminRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Find and delete the default admin entry from the admin table
+        AdminModel defaultAdmin = adminRepository.findByEmail("admin@gmail.com");
+        if (defaultAdmin != null) {
+            adminRepository.delete(defaultAdmin);
+        }
+
+        // Find and delete the default user entry from the user table
+        UserModel defaultUser = userRepository.findByEmail("admin@gmail.com");
+        if (defaultUser != null) {
+            userRepository.delete(defaultUser);
+        }
+    }
+};
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import com.example.backend.Model.AdminModel;
+import com.example.backend.Model.UserModel;
+import com.example.backend.Repository.AdminRepository;
+import com.example.backend.Repository.UserRepository;
+
+@Component
+public class DataLoader implements CommandLineRunner {
+
+	@Autowired
+    private AdminRepository adminRepository;
+	@Autowired
+	private UserRepository userRepository;
+
+//    @Autowired
+//    public DataLoader(AdminRepository adminRepository) {
+//        this.adminRepository = adminRepository;
+//    }
+//    
+//    @Autowired
+//    public DataLoader(UserRepository userRepository) {
+//        this.userRepository = userRepository;
+//    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Create an AdminModel object with default data
+        AdminModel admin = new AdminModel();
+        admin.setUsername("admin");
+        admin.setEmail("admin@gmail.com");
+        admin.setPassword("Admin@321");
+        admin.setMobileNumber("9876543210");
+        admin.setUserRole("admin");
+
+        // Save the AdminModel object to the database
+        adminRepository.save(admin);
+        
+        
+        UserModel user = new UserModel();
+        user.setUsername("admin");
+        user.setEmail("admin@gmail.com");
+        user.setPassword("Admin@321");
+        user.setMobileNumber("9876543210");
+        user.setUserRole("admin");
+
+
+        // Save the UserModel object to the database
+        userRepository.save(user);
+        
+    }
+}
