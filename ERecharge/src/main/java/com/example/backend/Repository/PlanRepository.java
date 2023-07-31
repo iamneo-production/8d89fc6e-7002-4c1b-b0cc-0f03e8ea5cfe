553cbbd3991be557f7e0e242399dd4e24f578e50
Sreevarshan S <101659129+Varshan00@users.noreply.github.com>
2023-07-31 21:09:04 +0530

    Delete ERecharge directory

--- ERecharge/src/main/java/com/example/backend/Repository/PlanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.Model.Plans;
-
-public interface  PlanRepository extends JpaRepository<Plans,Integer> {
-
-}
--- ERecharge/src/main/java/com/example/backend/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.backend.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.backend.Model.UserModel;
-
-public interface UserRepository extends JpaRepository<UserModel, Integer> {
-    UserModel findByEmail(String email);
-}
