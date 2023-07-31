9607213c1cda65750412095c2b4992eb519eb24f
Sreevarshan S <101659129+Varshan00@users.noreply.github.com>
2023-07-31 21:36:55 +0530

    Create SpringBootApplication.java

---
+++ springapp/src/main/java/com/examly/springapp/SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.examly.springapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ERechargeApplication.class, args);
+	}
+
+}
